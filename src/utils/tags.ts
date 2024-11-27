0b710214ee29c59483c06dd1fda105662d526539
IsraelSI11 <elplayerhd@hotmail.es>
2024-11-27 11:51:43 +0100

    Seccion de proyecto primera versión

---
+++ src/utils/tags.ts
@@ -0,0 +1,81 @@
+import AngularIcon from "../components/icons/AngularIcon.astro";
+import DockerIcon from "../components/icons/DockerIcon.astro";
+import FirebaseIcon from "../components/icons/FirebaseIcon.astro";
+import JavaIcon from "../components/icons/JavaIcon.astro";
+import JestIcon from "../components/icons/JestIcon.astro";
+import MariaDBIcon from "../components/icons/MariaDBIcon.astro";
+import NodeIcon from "../components/icons/NodeIcon.astro";
+import PostgreSqlIcon from "../components/icons/PostgreSqlIcon.astro";
+import ReactIcon from "../components/icons/ReactIcon.astro";
+import ShadcnIcon from "../components/icons/ShadcnIcon.astro";
+import SpringIcon from "../components/icons/SpringIcon.astro";
+import TailwindIcon from "../components/icons/TailwindIcon.astro";
+import TypescriptIcon from "../components/icons/TypescriptIcon.astro";
+
+export const TAGS = {
+    TAILWIND: {
+        name: "Tailwind CSS",
+        class: "bg-[#003159] text-white",
+        icon: TailwindIcon,
+    },
+    REACT: {
+        name: "React",
+        class: "bg-[#61DAFB] text-white",
+        icon: ReactIcon,
+    },
+    TYPESCRIPT: {
+        name: "TypeScript",
+        class: "bg-[#3178C6] text-white",
+        icon: TypescriptIcon,
+    },
+    NODEJS: {
+        name: "Node.js",
+        class: "bg-[#339933] text-white",
+        icon: NodeIcon,
+    },
+    MARIADB: {
+        name: "MariaDB",
+        class: "bg-[#003545] text-white",
+        icon: MariaDBIcon,
+    },
+    JEST: {
+        name: "Jest",
+        class: "bg-[#99425B] text-white",
+        icon: JestIcon,
+    },
+    DOCKER: {
+        name: "Docker",
+        class: "bg-[#2496ED] text-white",
+        icon: DockerIcon,
+    },
+    ANGULAR: {
+        name: "Angular",
+        class: "bg-[#DD0031] text-white",
+        icon: AngularIcon,
+    },
+    JAVA: {
+        name: "Java",
+        class: "bg-[#007396] text-white",
+        icon: JavaIcon,
+    },
+    SPRING: {
+        name: "Spring",
+        class: "bg-[#6DB33F] text-white",
+        icon: SpringIcon,
+    },
+    SHADCN: {
+        name: "Shadcn",
+        class: "bg-zinc-800 text-white",
+        icon: ShadcnIcon,
+    },
+    FIREBASE: {
+        name: "Firebase",
+        class: "bg-[#FFCA28] text-white",
+        icon: FirebaseIcon,
+    },
+    POSTGRESQL: {
+        name: "PostgreSQL",
+        class: "bg-[#336791] text-white",
+        icon: PostgreSqlIcon,
+    },
+};
